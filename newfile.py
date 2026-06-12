a831334292f2209cc9a6525cabea6fbee3e87107
Blessy-commit <blessybamigbade@gmail.com>
2026-06-12 21:38:04 +0200

    Delete newfile.py

--- newfile.py
+++
@@ -1,7 +0,0 @@
-class Student:
-    def_init_(self,name,age):
-        self.name = name
-        self.age =age
-        student1=Student("Demakatso",22)
-        print(student1.name)
-        print(student1.age)
