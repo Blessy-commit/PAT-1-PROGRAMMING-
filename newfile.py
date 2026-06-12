fb61359000b06f88eea3d07aaed1b62250208d22
Blessy-commit <blessybamigbade@gmail.com>
2026-06-12 21:36:56 +0200

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,7 @@
+class Student:
+    def_init_(self,name,age):
+        self.name = name
+        self.age =age
+        student1=Student("Demakatso",22)
+        print(student1.name)
+        print(student1.age)
